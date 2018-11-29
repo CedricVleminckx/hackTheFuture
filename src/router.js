aab76498de3de3515a0eb0351c23af941c595565
Michiel Tondeur <michiel.tondeur@student.odisee.be>
2018-11-29 09:28:25 +0100

    Added router

--- src/router.js
+++ src/router.js
@@ -1,24 +1,33 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import challenge1 from './views/challenge1.vue';
+import challenge2 from './views/challenge2.vue';
+import challenge3 from './views/challenge3.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/challenge1',
+            name: 'challenge1',
+            component: Challenge1
+        },
+        {
+            path: '/challenge2',
+            name: 'challenge2',
+            component: Challenge2
+        },
+        {
+            path: '/challenge3',
+            name: 'challenge3',
+            component: Challenge3
+        }
+    ]
 });
